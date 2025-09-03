4668ae450dd552b6258a6b929f0e89fd824a3a2a
Jotaerri17 <joaorafaelsantos17@gmail.com>
2025-09-03 10:51:18 -0300

    Add lucide-react dependency and update layout and page components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
-
+import { MapPin, TextAlignJustify, SunMedium, Moon, CalendarDays, Cloud, CloudRain, Wind, Droplest   } from "lucide-react";
 
 export default function Home() {
-  
+  return (
+
+    <div className="w-full h-screen bg-gradient-to-br from-gray-700 to-gray-900">
+
+    </div>
+  )
     
 }
